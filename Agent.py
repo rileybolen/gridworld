e0f93fff133973d8c4f2af38d78e568f08c3cd5b
Riley Bolen <riley@fireflower.io>
2020-11-21 15:13:06 -0700

    Add iterative policy improvement via monte carlo

--- Agent.py
+++ Agent.py
@@ -1,21 +1,46 @@
 import random
-
+import operator
 
 class Agent:
 
     def __init__(self, world):
-        self.position = [3,1]
+        self.position = [6,1]
         self.world = world
-        self.policy = {}
-        self.create_initial_policy()
+        # self.policy = {}
+        # self.create_initial_policy()
+
+        self.expected_rewards = {}
+        self.observations = {}
+        self.init_expected_rewards()
+
+        self.actions_taken = {}
 
     def act(self):
         actions = self.world.actions(self.position)
         self.position, reward = self.world.move(self.position, self.choose_action(self.position, actions))
-        if reward != 0:
+        if reward == 1 or reward == -1:
             return True, reward
         else:
-            return False, 0
+            return False, reward
+
+    def choose_action(self, position, actions):
+        chosen_action = random.choice(actions)
+        self.record_action(position, chosen_action)
+        return chosen_action
+
+    def record_action(self, position, action):
+        self.actions_taken['({0},{1}),({2},{3})'.format(position[0], position[1], action[0], action[1])] = \
+                {
+                    "position": '({0},{1})'.format(position[0], position[1]),
+                    "action": '({0},{1})'.format(action[0], action[1]),
+                }
+
+    def update_policy(self, reward):
+        for key, dict in self.actions_taken.items():
+            n = self.observations[dict["position"]][dict["action"]] + 1
+            mean = self.expected_rewards[dict["position"]][dict["action"]]
+            self.expected_rewards[dict["position"]][dict["action"]] = (mean * ((n-1)/n)) + (reward * (1/n))
+            self.observations[dict["position"]][dict["action"]] = n
 
     def create_initial_policy(self):
         for r in range(0, len(self.world.grid)):
@@ -26,9 +51,50 @@ class Agent:
                     for action in actions:
                         self.policy['({0},{1})'.format(r, c)]['({0},{1})'.format(action[0],action[1])] = 1 / len(actions)
 
-    def choose_action(self, position, actions):
-        action_weights = []
-        for action in actions:
-            action_weights.append(
-                self.policy['({0},{1})'.format(position[0], position[1])]['({0},{1})'.format(action[0], action[1])])
-        return random.choices(actions, weights=action_weights)[0]
+    def init_expected_rewards(self):
+        for r in range(0, len(self.world.grid)):
+            for c in range(0, len(self.world.grid[r])):
+                if not self.world.grid[r][c]:
+                    actions = self.world.actions([r,c])
+                    self.expected_rewards['({0},{1})'.format(r, c)] = {}
+                    self.observations['({0},{1})'.format(r, c)] = {}
+                    for action in actions:
+                        self.expected_rewards['({0},{1})'.format(r, c)]['({0},{1})'.format(action[0],action[1])] = 0
+                        self.observations['({0},{1})'.format(r, c)]['({0},{1})'.format(action[0], action[1])] = 0
+
+    def reset(self):
+        self.actions_taken = {}
+        self.position = [3,1]
+
+    def display_policy(self):
+        print("\nBest Action")
+        for r in range(0, len(self.world.grid)):
+            display_row = ""
+            for c in range(0, len(self.world.grid[r])):
+                if not self.world.grid[r][c]:
+                    if self.world.rewards[r][c] == 1:
+                        display_row += " W "
+                    elif self.world.rewards[r][c] == -1:
+                        display_row += " F "
+                    else:
+                        expected_action = max(self.expected_rewards['({0},{1})'.format(r, c)].items(),
+                                              key=operator.itemgetter(1))[0]
+                        action_text = {"(1,0)": "D", "(-1,0)": "U", "(0,1)": "R", "(0,-1)": "L"}
+                        display_row += " " + action_text[expected_action] + " "
+                else:
+                    display_row += "|||"
+            print(display_row)
+        print("\n")
+        print("Expected Reward")
+        for r in range(0, len(self.world.grid)):
+            display_row = ""
+            for c in range(0, len(self.world.grid[r])):
+                if not self.world.grid[r][c]:
+                    expected_action = max(self.expected_rewards['({0},{1})'.format(r, c)].items(),
+                                          key=operator.itemgetter(1))[0]
+                    display_row += " {:5.2f} ".format(self.expected_rewards['({0},{1})'.format(r, c)][expected_action])
+                else:
+                    display_row += "|||||||"
+            print(display_row)
+
+
--- index.py
+++ index.py
@@ -4,13 +4,17 @@ from Agent import Agent
 world = World()
 agent = Agent(world)
 
-done = False
-result = 0
+for e in range(5000):
+    done = False
+    result = 0
+    while not done:
+        done, result = agent.act()
+    agent.update_policy(result)
+    agent.reset()
+    #
+    # if result > 0:
+    #     print("Agent has won :)")
+    # else:
+    #     print("Agent has lost :(")
 
-while not done:
-    done, result = agent.act()
-
-if result > 0:
-    print("Agent has won :)")
-else:
-    print("Agent has lost :(")
+agent.display_policy()
